52e10be8f32f64b1b35d003f4b6910701e0bea85
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-01-27 17:59:00 +0530

    added path

--- index.js
+++ index.js
@@ -2,9 +2,18 @@ const express = require("express")
 const app = express()
 const dotenv = require("dotenv")
 const {userRouter} = require("./routers/userRouter")
+const cors = require("cors")
+const path = require("path")
 dotenv.config();
 require("./db/conn.js")
 const port = process.env.port || 5000
+app.use(
+    cors({
+        credentials: true,
+        origin: ["http://localhost:3000"],
+        methods: ["GET","POST"]
+    })
+)
 app.use(express.static(path.join(__dirname, "todo", "build"))); // static will tell kya what we used like css js files
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "todo", "build", "index.html")); //which ui to show which index.html file to pick
